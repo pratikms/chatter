694d7b2943e298f277a9f9c7c27259d278413aa4
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2021-10-06 02:10:12 +0530

    Add client and room

--- main.go
+++ main.go
@@ -26,7 +26,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	r := newRoom()
+
 	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// run the room
+	go r.run()
 
 	log.Println("Starting server...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
