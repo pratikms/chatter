27d5772a741a800250e6728f109d02459fd0756b
Pratik Shivaraikar <pshivaraikar@lenovo.com>
2021-10-06 00:38:25 +0530

    Add custom handler

--- main.go
+++ main.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-			<html>
-				<head>
-					<title>Chatter</title>
-				</head>
-				<body>
-					Let's chat
-				</body>
-			</html>
-		`))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	// templ represents a single template
+	templ *template.Template
+}
+
+// ServeHTTP handles the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		log.Println("Setting template once")
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
+	log.Println("Executing template")
+	t.templ.Execute(w, nil)
+}
+
+func main() {
+	http.Handle("/", &templateHandler{filename: "chat.html"})
 
 	log.Println("Starting server...")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
