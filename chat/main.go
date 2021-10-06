081955acc01e9fbd3532ded3dad806212195fcb4
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2021-10-07 02:31:55 +0530

    Add chat root

--- chat/main.go
+++ chat/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"html/template"
 	"log"
 	"net/http"
@@ -22,10 +23,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
 	})
 	log.Println("Executing template")
-	t.templ.Execute(w, nil)
+	t.templ.Execute(w, r)
 }
 
 func main() {
+	var addr = flag.String("addr", ":8080", "The address of the application")
+	flag.Parse()
+
 	r := newRoom()
 
 	http.Handle("/", &templateHandler{filename: "chat.html"})
@@ -34,8 +38,8 @@ func main() {
 	// run the room
 	go r.run()
 
-	log.Println("Starting server...")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	log.Printf("Starting server on %s\n...", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
