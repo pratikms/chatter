bbafb2652a1415f9ce5b0bc0a30fde171fabf1b8
Pratik Shivaraikar <pratik.shivaraikar@gmail.com>
2021-10-09 01:43:52 +0530

    Add tracer to chat

--- chat/main.go
+++ chat/main.go
@@ -5,8 +5,11 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
+
+	"github.com/pratikms/chatter/trace"
 )
 
 type templateHandler struct {
@@ -31,6 +34,7 @@ func main() {
 	flag.Parse()
 
 	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
 
 	http.Handle("/", &templateHandler{filename: "chat.html"})
 	http.Handle("/room", r)
--- chat/room.go
+++ chat/room.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
+	"github.com/pratikms/chatter/trace"
 )
 
 const (
@@ -21,6 +22,8 @@ type room struct {
 	leave chan *client
 	// clients hold all current clients in this room
 	clients map[*client]bool
+	// tracer will receive trace information of activity in the room
+	tracer trace.Tracer
 }
 
 var upgrader = &websocket.Upgrader{ReadBufferSize: socketBufferSize, WriteBufferSize: socketBufferSize}
@@ -31,16 +34,20 @@ func (r *room) run() {
 		case client := <-r.join:
 			// joining
 			r.clients[client] = true
+			r.tracer.Trace("New client joined")
 
 		case client := <-r.leave:
 			// leaving
 			delete(r.clients, client)
 			close(client.send)
+			r.tracer.Trace("Client left")
 
 		case msg := <-r.forward:
+			r.tracer.Trace("Message received: ", string(msg))
 			// forward message to all clients
 			for client := range r.clients {
 				client.send <- msg
+				r.tracer.Trace(" -- sent to client")
 			}
 		}
 	}
